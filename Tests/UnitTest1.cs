70a8afed98b1255a81a50a3b12cb615903bec937
Nikita Myzdrikov <nikita.myzdrikov@koniglabs.ru>
2023-06-14 16:45:10 +0300

    Add dummy test project

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Temporal solution for check tests
+        /// </summary>
+        [TestMethod]
+        public void FakeTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}

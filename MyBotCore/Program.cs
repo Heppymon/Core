af99bf2f371c1e511036d675ebf2c7b2fb09805c
nikita.myzdrikov <nikita.myzdrikov@poket.tech>
2025-10-16 17:41:31 +0300

    rebuild

--- MyBotCore/Program.cs
+++ MyBotCore/Program.cs
@@ -7,7 +7,6 @@ namespace MyBotCore
 {
     internal class Program
     {
-
         public static void Main(string[] args)
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Local";
